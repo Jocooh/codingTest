a432d4dbe7dd2afec6f5e5d4ca35de8069cfde84
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-09 11:26:32 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.js
@@ -0,0 +1,8 @@
+function solution(message) {
+   let answer= message.split("").length
+   return answer *2
+}
+
+//let str = 'Hello world, Java';
+//str = str.replace('Java', 'JavaScript');
+//console.log(str);

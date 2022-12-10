13cf7213debb5e14cf4cc29179ada56e1da509ac
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-10 17:11:40 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,8 +1,4 @@
 function solution(num_list) {
-    var answer = [];
-    for(let i=num_list.length-1;i>=0;i--){
-        answer.push(num_list[i]);
-    }
-    return answer;
+    return num_list.reverse()
 }
 //1.i= num_list.length를 -1로주고 반대로 뒤집어본다.

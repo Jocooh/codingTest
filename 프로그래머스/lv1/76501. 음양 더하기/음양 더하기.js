9a92f6c6b47633066a446aa6642ed5643ab329ce
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-10 12:26:08 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,8 +1,3 @@
 function solution(absolutes, signs) {
-    var answer = 0;
-    for(i=0;i<absolutes.length;i++) {
-        answer += absolutes[i] * (signs[i] ? 1 : -1);
-    }
-
-    return answer;
-}
+    return absolutes.reduce((a,s,i)=>a+(s*(signs[i]?1:-1)),0) 
+};

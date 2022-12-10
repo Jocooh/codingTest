0276e957707c5d98dafba44444539dc3b2e505f8
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-10 11:52:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,5 @@
-function solution(n){
-    var answer = n.toString().split('').reduce((a,b)=>(a += parseInt(b)),0);
-    return answer;
+function solution(n){  
+ var answer = n.toString().split('').reduce((a,b)=>(a += parseInt(b)),0);
+   return answer
+    console.log(answer) 
 }
-//1.reduce 사용
-solution(123);

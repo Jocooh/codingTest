d9d6e8d5cf57e0e8873aee4d7558a6fc529c11a0
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-13 13:26:31 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,7 +1,6 @@
 function solution(n) {
     var answer = [];
-    for(let i =1 ; i<= n ;i++){
-        if(i%2===1)answer.push(i)
-    }
-    return answer;
+    for(let i =1 ; i<=n ; i++){
+      if(i%2===1){answer.push(i)} 
+}return answer;
 }

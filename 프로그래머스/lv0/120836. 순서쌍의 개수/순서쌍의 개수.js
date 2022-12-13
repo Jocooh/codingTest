3fc2a3fc29a67f15aa68ee85893ea76cf1452176
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-13 13:33:13 +0900

    [level 0] Title: 순서쌍의 개수, Time: 5.39 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,8 +1,7 @@
 function solution(n) {
-var answer = 0;
-
-for(let i = 2; i <= n; i++) {
-    if(n % i === 0) answer++;
+    let answer = 0;
+    for(let i = 2 ; i<=n ; i++){
+        if(n%i===0){answer++}
+    }return answer +1
 }
-return answer + 1;
-}
+// 나누기 시 나머지가 0인 친구들을 가져오면된다.

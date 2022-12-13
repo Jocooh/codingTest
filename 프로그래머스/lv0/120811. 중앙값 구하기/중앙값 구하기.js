b5811815799d016598f01f93dbf67bd0aad2c9bd
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-13 13:20:33 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,8 +1,5 @@
-function solution(array) { 
-   let  newArr=array.sort(function (a,b){return a-b})
-    let answer=(Math.round(newArr.length/2))-1
-   return newArr[answer]
-}
-//length의 수에서 반올림한 수가 인덱스 번호가 되고 그 자리에 해당하는 값이 나오면 된다.
-//오름차순을 구현하기 위해서 
-
+function solution(array) {
+   let newArr = array.sort(function(a,b){return a-b});
+    let index=Math.floor(newArr.length / 2)
+    return newArr[index]
+    }

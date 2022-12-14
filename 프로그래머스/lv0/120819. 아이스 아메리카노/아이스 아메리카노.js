216fbfb4e8bff079cff4b38712a08b93447ae6c9
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-14 12:07:40 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,8 +1,4 @@
 function solution(money) {
-    if(money >0) {
-        let i = parseInt(money/5500);
-        let j = money-(5500*i);
-        let result=[i, j]
-        return result;
-    }
+    let arr=[parseInt(money/5500),money%5500]
+    return arr
 }

45e4bbf6332bb6bebbd950950f6c58d7f639cb3b
JOCO <111356420+Jocooh@users.noreply.github.com>
2022-12-14 11:55:44 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,9 +1,3 @@
 function solution(dot) {
-  let i = dot[0]>0? 1:0
-  let j = dot[1]>0? 1:0   //1:+ 0:-
-    if(i===1 && j===1){answer= 1}
-    else if(i===1 && j===0){answer = 4}
-    else if(i===0 && j===1){answer = 2}
-    else{answer = 3}
-   return answer 
+ return dot[0]>0? dot[1]>0 ? 1:4:dot[1]>0?2:3
 }

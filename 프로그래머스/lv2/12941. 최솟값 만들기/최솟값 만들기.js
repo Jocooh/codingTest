feb9bd06b6dfa451f17b83ddb989c36dfc60f01c
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-05-07 16:37:58 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.76 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,9 +1,5 @@
-function solution(a,b){
-   var answer = 0;
-   let Aarr = a.sort((a,b)=>a-b)
-   let Barr = b.sort((a,b)=>b-a)
-   for(let i= 0;i<a.length;i++){
-    answer+=Aarr[i]*Barr[i]
-  }
-    return answer;
+function solution(A,B){
+    A.sort((a, b) => a - b)
+    B.sort((a, b) => b - a)
+    return A.reduce((total, val, idx) => total + val * B[idx], 0)
 }

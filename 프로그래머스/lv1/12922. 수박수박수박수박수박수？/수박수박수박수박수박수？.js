8cf975ab77900e9a28003fccf425ac5fa0eb3f16
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-05-19 10:01:21 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  let answer = '';
+  for (let i = 0; i < n; i++) {
+    //n횟수만큼 반복
+    if (answer === '') {
+      answer += '수';
+    } else if (answer[answer.length - 1] === '수') answer += '박';
+    else {
+      answer += '수';
+    }
+  }
+  return answer;
+}
+console.log(solution(4));

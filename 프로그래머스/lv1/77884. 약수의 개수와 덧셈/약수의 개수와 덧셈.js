24a3df6829065f5e8255913554576178ead3245d
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-05-19 21:08:53 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,14 +1,11 @@
 function solution(left, right) {
-  var answer = 0;
-  for (let num = left; num <= right; num++) {
-    divisorCounter(num) % 2 === 0 ? (answer += num) : (answer -= num);
-  }
-  return answer;
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
 }
-
-const divisorCounter = (num) => {
-  let count = 0;
-  const sqrt = Math.sqrt(num);
-  for (let i = 1; i <= sqrt; i++) if (num % i === 0) count += 1;
-  return Number.isInteger(sqrt) ? (count - 1) * 2 + 1 : count * 2;
-};

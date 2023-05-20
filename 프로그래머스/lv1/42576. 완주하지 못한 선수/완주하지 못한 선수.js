d3fed677f1f73943cfc7421b1607e37ad575c874
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-05-20 21:00:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.25 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,17 @@
 function solution(participant, completion) {
-  participant = participant.sort();
-  completion = completion.sort();
-  for (let i = 0; i < completion.length; i++) {
-    if (participant[i] !== completion[i]) return participant[i];
-  }
-  return participant[participant.length - 1];
+    const map = new Map();
+
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
+    }
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    return 'nothing';
 }
